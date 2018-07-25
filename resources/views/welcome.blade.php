0b89f83943aa84af7570cead16edb230a483435f
jypin <apple80177@gmail.com>
2018-07-25 22:32:35 +0800

    Laravel example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel
+                    Laravel Example
                 </div>
 
                 <div class="links">
