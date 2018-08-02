1220abb7af48a0594748bf2373a96234d1d0553a
jypin <apple80177@gmail.com>
2018-08-02 12:19:44 +0800

    all routes is added.

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,42 @@ Route::get('/', function () {
 
 Route::get('/plus', 'ToDoController@plus');
 
-//使用者註冊頁面
-Route::get('/user/auth/sign-up');
-//使用者資料新增
-Route::post('/user/auth/sign-up');
-//使用者登入頁面
-Route::get('/user/auth/sign-in');
-//使用者登入處理
-Route::post('/user/auth/sign-in');
-//使用者登出
-Route::get('/user/auth/sign-out');
+Route::group(['prefix' => 'user'], function(){
+    Route::group(['prefix' => 'auth'], function(){
+        //使用者註冊頁面
+        Route::get('/sign-up', 'User\UserAuthController@signUpPage');
+        //使用者資料新增
+        Route::post('/sign-up', 'User\UserAuthController@signUpProcess');
+        //使用者登入頁面
+        Route::get('/sign-in', 'User\UserAuthController@signInPage');
+        //使用者登入處理
+        Route::post('/sign-in', 'User\UserAuthController@signInProcess');
+        //使用者登出
+        Route::get('/sign-out', 'User\UserAuthController@signOut');
+    });
 
-//商品清單檢視
-Route::get('/merchandise');
-//商品管理清單檢視
-Route::get('/merchandise/manage');
-//商品資料新增
-Route::get('/merchandise/create');
-//商品單品檢視
-Route::get('/merchandise/{merchandise_id}');
-//商品單品編輯頁面檢視
-Route::get('/merchandise/{merchandise_id}/edit');
-//商品單品資料修改
-Route::put('/merchandise/{merchandise_id}');
-//購買商品
-Route::post('/merchandise/{merchandise_id}/buy');
+});
+
+
+Route::group(['prefix' => 'merchandise'], function(){
+    //商品清單檢視
+    Route::get('/', 'Merchandise\MerchandiseController@merchandiseListPage');
+    //商品管理清單檢視
+    Route::get('/manage', 'Merchandise\MerchandiseController@merchandiseManageListPage');
+    //商品資料新增
+    Route::get('/create', 'Merchandise\MerchandiseController@merchandiseCreateProcess');
+    Route::group(['prefix' => '{merchandise_id}'], function(){
+        //商品單品檢視
+        Route::get('/', 'Merchandise\MerchandiseController@merchandiseItemPage');
+        //商品單品編輯頁面檢視
+        Route::get('/edit', 'Merchandise\MerchandiseController@merchandiseItemEditPage');
+        //商品單品資料修改
+        Route::put('/', 'Merchandise\MerchandiseController@merchandiseItemUpdateProcess');
+        //購買商品
+        Route::post('/buy', 'Merchandise\MerchandiseController@merchandiseItemBuyProcess');
+    });
+});
+
 //交易紀錄頁面檢視
-Route::get('/transaction');
+Route::get('/transaction', 'Transaction\TransactionController@transactionListPage');
 
